1ed951ab5811d2057692b0804b6237773a039577
Zody <Zoraida1988@yahoo.com>
2021-10-06 18:46:57 -0500

    ProductRepository

---
+++ ProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ZoraidaDesigns
+{
+    public class ProductRepository
+    {
+        private readonly IDbConnection _conn;
+        public ProductRepository(IDbConnection conn)
+        {
+            _conn = conn
+        }
+
+    }
+}
