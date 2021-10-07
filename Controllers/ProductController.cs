af185900d2ff2cb6fdd8018c80866646c3fb9cde
Zody <Zoraida1988@yahoo.com>
2021-10-06 19:17:21 -0500

    ProductController

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ZoraidaDesigns.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductRepository repo;
+
+        public ProductController(IProductRepository repo)
+        {
+            this.repo = repo;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
